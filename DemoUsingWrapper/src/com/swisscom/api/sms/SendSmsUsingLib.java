d2e7fd602b563249c47934135d1ca599dd771136
Kay Lummitsch <kay.lummitsch@swisscom.com>
2014-04-28 14:45:52 +0200

    Typo fixed ...

--- DemoUsingWrapper/src/com/swisscom/api/sms/SendSmsUsingLib.java
+++ DemoUsingWrapper/src/com/swisscom/api/sms/SendSmsUsingLib.java
@@ -6,7 +6,7 @@ public class SendSmsUsingLib {
 	public static void main(String[] args) {
 		
 		SendSMS.sendSms(
-				"<YOUR_CLIENT_ID_GOES_HERE">, 
+				"<YOUR_CLIENT_ID_GOES_HERE>", 
 				"+41791544781", 
 				"+41791544781", 
 				"Hi from sendSMS(GSMA) called by Java client - Using a library:-)");
